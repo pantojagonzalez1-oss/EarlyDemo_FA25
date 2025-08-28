d9beda547e700fee4f9867b35212fb2562014442
Miriam <Miriam@MIRIAMPANTOJA01>
2025-08-28 10:40:56 -0500

    Add project files.

---
+++ EarlyDemo_FA25/Main.cpp
@@ -0,0 +1,19 @@
+// This is a single line comment 
+
+/*
+This 
+is 
+a multi-line comment 
+*/
+
+// Including necessary libraries
+// These are preprocessor directives
+#include <iostream> // For input and output 
+
+	// The main function - execution starts here
+int main()
+{
+	//Print a message to the console
+	std::cout << "Hello, World!" << std::endl; // End the line after printing
+	return 0; // Indicate that the program ended successfully
+}
